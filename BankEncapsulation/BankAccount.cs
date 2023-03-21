354d0c69e081f4d56f5887205e7197dbd4463ae7
Victoria Pham <victoriapham52@gmail.com>
2023-03-20 20:52:19 -0700

    Exercise commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+		public BankAccount()
+		{
+		}
+
+		private double _balance = 0;
+
+
+		public void Deposit(double amount)
+		{
+			_balance += amount;
+		}
+
+		public double GetBalance()
+		{
+			return _balance;
+		}
+
+     
+
+
+    }
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var victoriaAccount = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            victoriaAccount.Deposit(amountToDeposit);
+                       
+
+            Console.WriteLine($"New account Balance is:${victoriaAccount.GetBalance()}");
+                   
+
+           
+
         }
     }
 }
